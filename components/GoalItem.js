9eccfe774ee2988fdcc8a6e2af99ea685a4e3672
Morgan Hondros <morganhondros@gmail.com>
2024-06-10 21:03:23 -0400

    Adding styling when goal item is pressed for deletion but for andiord and ios.

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,7 +2,11 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+    <Pressable
+      android_ripple={{ color: "#210644" }}
+      onPress={props.onDeleteItem.bind(this, props.id)}
+      style={({ pressed }) => pressed && styles.pressedItem}
+    >
       <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.text}</Text>
       </View>
@@ -18,6 +22,9 @@ const styles = StyleSheet.create({
     padding: 8,
     borderRadius: 6,
     backgroundColor: "dodgerblue",
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
   goalText: {
     color: "white",
